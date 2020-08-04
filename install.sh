4ba757845fd7b8a0a310765abbc92c0c019b3756
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-08-04 03:44:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #Creat filesystems
 
+
+mkfs.btrfs -L root /dev/sda3
+
 #Set defaults (Grund: wenn /dev/sda3 gemoutet wird, wird automatisch das subvol als default genommen!)
 
 #Start installer
 
 #Mount /target
+mount -t btrfs /dev/sda3 /target
+mount -t btrfs -o subvol=@ /dev/disk/by-label/root /target
 
 
