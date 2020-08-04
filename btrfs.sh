7f1b845fef46d506d495a690e19b18e839d589c7
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-08-04 04:53:17 +0200

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -8,8 +8,9 @@ btrfs subvol snapshot -r / /snapshots/root.$(date +%Y-%m-%d_%Hh%Mm)
 Create a readonly snapshot of '/' in '/snapshots/root.20160919-0954'
 
 #restore
-btrfs sub del /
-btrfs subvol snapshot -r / /snapshots/root.$(date +%Y-%m-%d_%Hh%Mm)
+mount -o subvol=/ /dev/sda3 /mnt
+btrfs sub del /mnt/@
+btrfs subvol snapshot /mnt/snapshots/root.$(date +%Y-%m-%d_%Hh%Mm) /mnt/@
 
 tree -L 3 /snapshots
 
@@ -28,13 +29,12 @@ btrfs subvolume list /
 #show all subvols systemwide:
 btrfs filesystem show
 
-
-
 #mounten
 mount -t btrfs -o subvolid=256,compress /dev/sda5 /mnt 
 
 #mount ROOT of all subvol:
-mount -o subvol=/ /dev/sda1 /mnt
+mount -o subvol=/ /dev/sda3 /mnt
+
 
 #creat subvol
 btrfs subvol create mynewrootsubvol
@@ -50,3 +50,14 @@ btrfs subvol get-default /
 
 btrfs subvolume set-default /mnt
 
+
+
+
+
+
+#Firewall snapshot
+mount -o subvol=/ /dev/sda3 /mnt
+btrfs subvol snapshot -r /mnt/@ /mnt/snapshots/root.$(date +%Y-%m-%d_%Hh%Mm)
+btrfs subvol list /
+tree -L 2 /mnt/
+umount /mnt
