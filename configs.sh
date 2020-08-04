428b135b88fbd869843a5b15c7493bf4659e87f1
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-08-05 01:55:22 +0200

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -1,19 +1,30 @@
 #mount überprüfen
 btrfs subvolume list .
+btrfs subvol get-default /
+btrfs subvolume show / | grep "Top level ID:"
+btrfs subvol set-default 5 /
+btrfs subvol get-default /
 
 
 nano /etc/fstab
 ------------------------------------------------------------------
+# /etc/fstab: static file system information.
+#
+# Use 'blkid' to print the universally unique identifier for a
+# device; this may be used with UUID= as a more robust way to name devices
+# that works even if disks are added and removed. See fstab(5).
+#
 # <file system> <mount point>   <type>  <options>       <dump>  <pass>
 
 #btrfs root subvol auf  /dev/sda3
-UUID=73dae494-f849-4867-8629-f5546deb968e /               btrfs   defaults,ssd,noatime,compress=zstd            0       0
+UUID=73dae494-f849-4867-8629-f5546deb968e /               btrfs   defaults,ssd,noatime,compress=zstd,subvol=/@          0       0
 # /boot/efi auf /dev/sda1
 UUID=8068-4872                            /boot/efi       vfat    umask=0077                                    0       1
 # swap auf /dev/sda2
 UUID=d2d5ff52-df49-46f7-bdfb-6b91ff53a2ff none            swap    sw                                            0       0
 #tmp
 tmpfs                                     /tmp            tmpfs   rw,nosuid,nodev                               0       0
+
 
 ------------------------------------------------------------------
 
